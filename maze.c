8a26869d395b259d2f1b650d84c07b9cd3ac2870
simal24k <simal.mpgss@gmail.com>
2024-11-25 20:45:37 +0500

    Update maze.c

--- maze.c
+++ maze.c
@@ -10,25 +10,23 @@ void generate_maze(char maze[MAZE_SIZE][MAZE_SIZE]) {
         }
     }
 
-    // TODO: Implement a proper maze generation algorithm
-    // For now, we'll just create some random paths
-
-    // Set start and end points
-    maze[0][0] = 'S';
-    maze[MAZE_SIZE-1][MAZE_SIZE-1] = 'E';
-
     // Create some random paths
     for (int i = 0; i < MAZE_SIZE * MAZE_SIZE / 3; i++) {
         int x = rand() % MAZE_SIZE;
         int y = rand() % MAZE_SIZE;
         maze[y][x] = ' ';
     }
+
+    // Set start and end points
+    maze[0][0] = 'S';
+    maze[MAZE_SIZE-1][MAZE_SIZE-1] = 'E';
 }
 
 void print_maze(char maze[MAZE_SIZE][MAZE_SIZE]) {
     for (int i = 0; i < MAZE_SIZE; i++) {
         for (int j = 0; j < MAZE_SIZE; j++) {
-            printf("%c", maze[i][j]);
+            // Add a space after each character for better visibility
+            printf("%c ", maze[i][j]);
         }
         printf("\n");
     }
